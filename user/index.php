655d123a252cb14fff3aaec034c9fae58abe5fd4
Ashrafi <ashhrafi098@outlook.com>
2023-12-29 20:21:12 +0530

    Minor Details

--- user/index.php
+++ user/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>User Registration</title>
+    <title>User Login</title>
     <!-- Add Bootstrap CSS link -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
