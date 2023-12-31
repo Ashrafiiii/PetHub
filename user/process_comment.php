eb67300f4c08e45a7053fa37222db6cbcf8a4650
Ashrafi <ashhrafi098@outlook.com>
2023-12-31 22:22:57 +0530

    added redirecting page

--- user/process_comment.php
+++ user/process_comment.php
@@ -22,16 +22,18 @@ if ($userResult && mysqli_num_rows($userResult) > 0) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $post_id = $_POST['post_id'];
+    $post_id = $_POST['po_id'];
     $comment = $_POST['comment'];
 
     // Insert the comment into the database
     $commentQuery = "INSERT INTO comments (po_id,u_id, comment, timestamp) VALUES ('$post_id', '$user_id','$comment', NOW())";
 
     if (mysqli_query($con, $commentQuery)) {
-        echo "Comment added successfully";
-        exit();
-    } else {
+        header("location: back_to_profile.php");
+    }       
+        
+     else 
+     {
         echo "Error adding comment: " . mysqli_error($con);
     }
 }
