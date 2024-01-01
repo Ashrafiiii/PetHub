4dc24fae6581864de7a0d5c821477a3fc57b41df
Ashrafi <ashhrafi098@outlook.com>
2024-01-02 00:49:25 +0530

    fixed vet rejection form

--- admin/reject_vet.php
+++ admin/reject_vet.php
@@ -7,7 +7,7 @@ if (isset($_GET['vet_id'])) {
     $vetId = $_GET['vet_id'];
 
     // Fetch vet data from temp_vet
-    $q = "SELECT * FROM temp_vet WHERE v_id = $vetId";
+    $q = "SELECT * FROM temp_vet WHERE vet_id = $vetId";
     $result = mysqli_query($con, $q);
     $vet = mysqli_fetch_assoc($result);
 
